e78be6d7804aac70c0483d230d62e75883269cd8
Zoltan Toth <zoltan.toth@liferay.com>
2020-11-28 10:28:44 +0100

    TZ-1 Project skeleton

---
+++ src/main/java/com/zoltan/toth/molecule/MoleculeApplication.java
@@ -0,0 +1,13 @@
+package com.zoltan.toth.molecule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoleculeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoleculeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zoltan/toth/molecule/MoleculeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoltan.toth.molecule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoleculeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
